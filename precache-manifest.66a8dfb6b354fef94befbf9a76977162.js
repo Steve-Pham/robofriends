471c81a4bb57030e18140ea047e43beafd830828
Steve Pham <stevepham@Steves-MBP.cgocable.net>
2019-06-29 23:44:46 -0400

    Updates

---
+++ precache-manifest.66a8dfb6b354fef94befbf9a76977162.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bbf4484f20f92469583b9d978a1bbe5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3cab8beee75e13c8622f",
+    "url": "/robofriends/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "5bb719f758c6f1fb2b5f",
+    "url": "/robofriends/static/css/main.3655682f.chunk.css"
+  },
+  {
+    "revision": "3cab8beee75e13c8622f",
+    "url": "/robofriends/static/js/2.eaa67066.chunk.js"
+  },
+  {
+    "revision": "5bb719f758c6f1fb2b5f",
+    "url": "/robofriends/static/js/main.4e9f1a92.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
